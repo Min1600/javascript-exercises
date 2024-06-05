db7c92a2eca66883359e52531fa01ac3d363b896
Min1600 <158844940+Min1600@users.noreply.github.com>
2024-06-05 22:25:26 +0100

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,10 @@
-const repeatString = function(word, times) {
-    let hold = "";
-    for (let i = 0; i < times; i++){
-      hold += word
-    };
-    return hold};
+const repeatString = function (word, times) {
+  if (times < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < times; i++) {
+    string += word;
+  }
+  return string;
+};
 // Do not edit below this line
 module.exports = repeatString;
