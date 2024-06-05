5fedb8150608f05e4c05987453dd74556240e364
Min1600 <158844940+Min1600@users.noreply.github.com>
2024-06-05 22:23:37 +0100

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,7 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
     let hold = "";
-    for (let i = 0; i < 3; i++){
-      hold += "hey"
+    for (let i = 0; i < times; i++){
+      hold += word
     };
     return hold}
 // Do not edit below this line
